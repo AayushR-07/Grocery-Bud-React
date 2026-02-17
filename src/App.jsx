09299bf918303d29caf2528d473ea4a38106a77b
Aayush Ranjitkar <aayushranjitkar120@gmail.com>
2026-02-17 19:40:28 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import Items from "./components/Items";
+import { groceryItems } from "./data/groceryItems";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
+import "./App.css";
+
+const App = () => {
+  const [items, setItems] = useState(groceryItems);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("grocery item added");
+  };
+
+  const editCompleted = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    toast.success("item deleted");
+  };
+
+   return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
+      <Items
+        items={items}
+        editCompleted={editCompleted}
+        removeItem={removeItem}
+      />
+    </section>
+  );
+};
+
+export default App;
